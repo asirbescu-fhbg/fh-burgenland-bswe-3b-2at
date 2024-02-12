606cfc392d736f202522dc0753e688421ecf9feb
asirbescu-fhbg <114183627+asirbescu-fhbg@users.noreply.github.com>
2024-02-12 22:45:54 +0100

    test(person): add unit test for person controller (issue: #27, pr: #35)

--- src/test/java/io/muehlbachler/fhburgenland/swm/examination/controller/PersonControllerTest.java
+++ src/test/java/io/muehlbachler/fhburgenland/swm/examination/controller/PersonControllerTest.java
@@ -1,6 +1,6 @@
 package io.muehlbachler.fhburgenland.swm.examination.controller;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import io.muehlbachler.fhburgenland.swm.examination.model.Note;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,6 +8,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import io.muehlbachler.fhburgenland.swm.examination.model.Person;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
 
 /**
  * Test class for {@code PersonController}.
@@ -22,7 +26,47 @@ public class PersonControllerTest {
         ResponseEntity<Person> person =
                 personController.get("81150016-8501-4b97-9168-01113e21d8a5");
 
-        assertEquals(HttpStatus.OK, person.getStatusCode(), "person should be found");
-        assertEquals("John", person.getBody().getFirstName(), "firstName should be John");
+        Assertions.assertEquals(HttpStatus.OK, person.getStatusCode(),
+                "person should be found");
+        Assertions.assertEquals("John",
+                person.getBody().getFirstName(),
+                "firstName should be John");
     }
+
+    @Test
+    void testCreatePerson() {
+        Person expected = new Person();
+        expected.setFirstName("Jonathan");
+        expected.setLastName("John");
+
+        Person actual = personController.create(expected);
+
+        Assertions.assertNotNull(actual);
+        Assertions.assertEquals(expected.getFirstName(), actual.getFirstName());
+        Assertions.assertNull(actual.getNotes());
+    }
+
+    @Test
+    void testGetByQuery() {
+        List<Person> expected = personController.list().subList(1, 2);
+        List<Person> actual = personController.query("Jane", "");
+        Assertions.assertEquals(expected.size(), actual.size());
+        Assertions.assertEquals(expected.getFirst().getFirstName(),
+                actual.getFirst().getFirstName());
+        Assertions.assertEquals(expected.getFirst().getLastName(),
+                actual.getFirst().getLastName());
+    }
+
+    @Test
+    void testCreateNoteIdNotFound() {
+        Note note = new Note();
+        note.setContent("Damn! This is a remarkable test");
+        ResponseEntity<Note> actual =
+                personController.createNote("1", note);
+
+        Assertions.assertEquals(HttpStatus.NOT_FOUND, actual.getStatusCode());
+        Assertions.assertNull(actual.getBody());
+    }
+
+
 }
