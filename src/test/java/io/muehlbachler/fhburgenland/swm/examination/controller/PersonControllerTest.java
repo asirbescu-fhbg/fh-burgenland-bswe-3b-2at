2f264be183677f09f1679018ec475071d52aadb7
Daniel Muehlbachler-Pietrzykowski <daniel@muehlbachler.io>
2024-02-16 08:11:49 +0100

    chore(ci): check pipeline

--- src/test/java/io/muehlbachler/fhburgenland/swm/examination/controller/PersonControllerTest.java
+++ src/test/java/io/muehlbachler/fhburgenland/swm/examination/controller/PersonControllerTest.java
@@ -21,6 +21,19 @@ public class PersonControllerTest {
     @Autowired
     private PersonController personController;
 
+    /**
+     * Tests the getbyID method using
+     * a known person ID to ensure it retrieves the person successfully.
+     */
+    @Test
+    void testGetByIdNotFound() {
+        ResponseEntity<Person> person =
+		personController.get("81150016-8501-4b97-9168-01113e21d8a2");
+
+        Assertions.assertEquals(HttpStatus.NOT_FOUND, person.getStatusCode(),
+		"person should not be found");
+    }
+
     @Test
     void testGetById() {
         ResponseEntity<Person> person =
